f66e10fde4c7a2f6479c473c19f0643834a30881
Masdikaa <masdikailhanmansiz@gmail.com>
2023-05-14 22:30:23 +0700

    Update menu mockup

--- app/src/main/java/com/himawan/pembelajarantunanetra/MainMenuActivity.kt
+++ app/src/main/java/com/himawan/pembelajarantunanetra/MainMenuActivity.kt
@@ -1,35 +1,42 @@
 package com.himawan.pembelajarantunanetra
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.himawan.pembelajarantunanetra.databinding.ActivityMainMenuBinding
 
-class MainMenuActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //Always LightTheme
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.cardHewan.setOnClickListener {
-            Toast.makeText(this, "Hewan", Toast.LENGTH_SHORT).show()
-        }
+        binding.cardHewan.setOnClickListener(this)
+        binding.cardMatematika.setOnClickListener(this)
+        binding.cardOrgan.setOnClickListener(this)
+        binding.cardBangunRuang.setOnClickListener(this)
 
-        binding.cardOrgan.setOnClickListener {
-            Toast.makeText(this, "Organ Dalam", Toast.LENGTH_SHORT).show()
-        }
+    }
 
-        binding.cardMatematika.setOnClickListener {
-            Toast.makeText(this, "Matematika", Toast.LENGTH_SHORT).show()
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.card_hewan -> {
+                Toast.makeText(this, "Menu Hewan", Toast.LENGTH_SHORT).show()
+            }
+            R.id.card_matematika -> {
+                Toast.makeText(this, "Menu Operasi Matematika", Toast.LENGTH_SHORT).show()
+            }
+            R.id.card_organ -> {
+                Toast.makeText(this, "Menu Organ Dalam", Toast.LENGTH_SHORT).show()
+            }
+            R.id.card_bangun_ruang -> {
+                Toast.makeText(this, "Menu Bangun Ruang", Toast.LENGTH_SHORT).show()
+            }
         }
-
-        binding.cardBangunruang.setOnClickListener {
-            Toast.makeText(this, "Bangun Ruang", Toast.LENGTH_SHORT).show()
-        }
-
     }
 }
