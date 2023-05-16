2411a220aa8172ac82c9e5b959a98a94799b4f68
Masdikaa <masdikailhanmansiz@gmail.com>
2023-05-16 11:23:45 +0700

    Add Home backsound music

--- app/src/main/java/com/himawan/pembelajarantunanetra/MainMenuActivity.kt
+++ app/src/main/java/com/himawan/pembelajarantunanetra/MainMenuActivity.kt
@@ -1,7 +1,9 @@
 package com.himawan.pembelajarantunanetra
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -11,6 +13,9 @@ import com.himawan.pembelajarantunanetra.databinding.ActivityMainMenuBinding
 class MainMenuActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainMenuBinding
+
+    private var mp: MediaPlayer? = null
+    private var backsoundHome: MutableList<Int> = mutableListOf(R.raw.backsound_home_menu)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.statusBarColor =
@@ -27,19 +32,43 @@ class MainMenuActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
+        val clickSound: MediaPlayer = MediaPlayer.create(this, R.raw.mouse_click)
         when (v.id) {
             R.id.card_hewan -> {
+                clickSound.start()
                 startActivity(Intent(this, HewanActivity::class.java))
             }
             R.id.card_matematika -> {
+                clickSound.start()
                 startActivity(Intent(this, MathActivity::class.java))
             }
             R.id.card_organ -> {
+                clickSound.start()
                 startActivity(Intent(this, OrgansActivity::class.java))
             }
             R.id.card_bangun_ruang -> {
+                clickSound.start()
                 startActivity(Intent(this, GeometryActivity::class.java))
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playSound(this.backsoundHome[0])
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mp !== null) mp?.pause()
+    }
+
+    private fun playSound(id: Int) {
+        if (mp == null) {
+            mp = MediaPlayer.create(this, id)
+            Log.d("MainMenuActivity", "ID = ${mp!!.audioSessionId}")
+        }
+        mp?.start()
+        mp?.isLooping = true
     }
 }
