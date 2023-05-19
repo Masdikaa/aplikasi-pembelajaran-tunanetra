0960465a9214ab731d63cd608d61d53b2a596597
Masdikaa <masdikailhanmansiz@gmail.com>
2023-05-19 14:28:47 +0700

    Adding voice over in GeometryActivity.kt

--- app/src/main/java/com/himawan/pembelajarantunanetra/GeometryActivity.kt
+++ app/src/main/java/com/himawan/pembelajarantunanetra/GeometryActivity.kt
@@ -1,5 +1,6 @@
 package com.himawan.pembelajarantunanetra
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,7 @@ class GeometryActivity : AppCompatActivity() {
     //Variable bangun ruang
     private lateinit var listNamaBangunRuang: Array<String>
     private lateinit var listImageBangunRuang: Array<Int>
+    private var soundVoiceOverGeometry: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.statusBarColor =
@@ -36,6 +38,8 @@ class GeometryActivity : AppCompatActivity() {
                 i = 5
             }
             Log.d("Index", i.toString())
+            soundVoiceOverGeometry?.stop()
+            soundVoiceOverGeometry?.reset()
             binding.imageBangunRuang.setImageResource(listImageBangunRuang[i])
             binding.tvNamaBangunRuang.text = listNamaBangunRuang[i]
         }
@@ -48,10 +52,61 @@ class GeometryActivity : AppCompatActivity() {
                 i = 5
             }
             Log.d("Index", i.toString())
+            soundVoiceOverGeometry?.stop()
+            soundVoiceOverGeometry?.reset()
             binding.imageBangunRuang.setImageResource(listImageBangunRuang[i])
             binding.tvNamaBangunRuang.text = listNamaBangunRuang[i]
         }
 
+        binding.btnPlayVoice.setOnClickListener {
+            when (i) {
+                0 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry = MediaPlayer.create(this, R.raw.cube_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+                1 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry = MediaPlayer.create(this, R.raw.cuboid_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+                2 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry =
+                        MediaPlayer.create(this, R.raw.pyramid_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+                3 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry =
+                        MediaPlayer.create(this, R.raw.triangular_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+                4 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry = MediaPlayer.create(this, R.raw.sphere_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+                5 -> {
+                    soundVoiceOverGeometry?.stop()
+                    soundVoiceOverGeometry?.reset()
+                    soundVoiceOverGeometry = MediaPlayer.create(this, R.raw.cone_desc_voice_gttx)
+                    soundVoiceOverGeometry?.start()
+                }
+            }
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundVoiceOverGeometry?.stop()
+        soundVoiceOverGeometry?.release()
     }
 
     private fun dataInitialize() {
